ef8da27a5eedd6a26fdd663f40fb461d67f3884f
cee <hungns126@gmail.com>
2021-05-14 20:15:01 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Login from './screens/Login'
+import Home from './screens/Home'
+
+function App() {
+  return (
+    <HashRouter>
+      {/* <React.Suspense fallback={loading}> */}
+      <Switch>
+        <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+        {/* <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} /> */}
+        {/* <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} /> */}
+        {/* <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} /> */}
+        {/* <Route path="/" name="Home" render={props => <TheLayout {...props}/>} /> */}
+        <Route path="/" name="Home" render={props => <Home {...props} />} />
+      </Switch>
+      {/* </React.Suspense> */}
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/screens/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function abc () {
+
+}
+
+function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
