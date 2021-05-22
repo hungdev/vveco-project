058eb77e210f77b23e709f016b9c473870ec51ff
cee <hungns126@gmail.com>
2021-05-22 09:53:03 +0700

    fix api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,11 +1,9 @@
 import { create } from 'axios';
 import { store } from './store';
-import Toast from 'react-native-toast-message';
-import { AlertDismiss } from '../components';
 import { reset } from "app/reducers/auth.reducer";
-
+// https://landapi.vipage.vn/vvlanding/auth/login
 const api = create({
-  baseURL: 'http://45.32.106.28:3000/',
+  baseURL: '',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
@@ -25,19 +23,9 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use((response) => response, (error) => {
   if (error.response.status === 401) {
     console.tron.log('401', error.response)
-    AlertDismiss({
-      message: error?.response?.data?.message || error?.response?.data?.errors?.[0]?.msg || 'Something went wrong!. Please login again',
-      onPress: () => store.dispatch(reset())
-    })
   }
   if (error.response.status === 400 || error.response.status === 404) {
     console.tron.log('error 400', error.response)
-    Toast.show({
-      type: 'error',
-      text1: 'Error!',
-      text2: error?.response?.data?.message || error?.response?.data?.errors?.[0]?.msg || 'Resource unavailable.',
-      visibilityTime: 5000,
-    });
   }
   return Promise.reject(error.response);
 });
